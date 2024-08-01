918ee7600ff44b112b4f836a7393047200e4b2ec
Hassan Hammoud <hassan.hammoud@montyholding.com>
2024-08-01 12:44:19 +0300

    Initial commit

---
+++ server/models/Blog.js
@@ -0,0 +1,25 @@
+// models/Blog.js
+
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
